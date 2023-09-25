4c97351f1a1478442a31c30e683f5828b19fb0f5
Armstrong <panzymanunlimited@hotmail.com>
2023-09-25 17:48:41 -0400

    finished the NOMAD Paste and Go NOMAD Table.

--- src/components/ModalTables/NOMADModalTable.js
+++ src/components/ModalTables/NOMADModalTable.js
@@ -18,7 +18,21 @@ import RCROptionMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADRCROptio
 import RFFOptionMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADRFFOptionMap'
 import WindVelocityOptionMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADWindVelocityOptionMap';
 import GustVelocityOptionMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADGustVelocityOptionMap';
-
+import VisibilityDistanceOptionMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADVisibilityDistanceOptionMap'
+import CeilingTypeOptionMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADCeilingTypeOptionMap'
+import CeilingAltitudeOptionMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADCeilingAltitudeOptionMap'
+import TemperatureOptionMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADTemperatureOptionMap'
+import DewPointOptionMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADDewPointOptionMap'
+import RWYMarkingsOptionMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADRWYMarkingOptionMap'
+import ARFFOptionMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADARFFCategoryOptionMap'
+import OnGroundOptionsMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADOnGroundOptionMap'
+import MHETypeOptionsMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADMHETypeOptionMap';
+import MHENumberOptionsMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADMHENumberOptionMap'
+import FuelTypeOptionsMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADFuelTypeOptionMap'
+import FuelQtyOptionsMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADFuelQtyOptionMap'
+import MedLevelAvailOptionsMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADMedLevelAvailOptionMap'
+import ITWCapabilityOptionsMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADITWCapabilityOptionMap'
+import SuitabilityCodeOptionMap from '../../IRISComponents/NOMAD/ChoiceOptions/NOMADSuitabilityCodeOptionMap'
 
 const ModalTable = ({
     fileContent,
@@ -230,6 +244,270 @@ const ModalTable = ({
                     </td>
                     <td>
                         <span>{getLabelFromValue(fileContent[27] + fileContent[28], GustVelocityOptionMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Visibility Distance
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[29], VisibilityDistanceOptionMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Ceiling Type
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[30], CeilingTypeOptionMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Ceiling Altitude
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[31] + fileContent[32], CeilingAltitudeOptionMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Temperature
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[33] + fileContent[34], TemperatureOptionMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Dew Point
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[35] + fileContent[36], DewPointOptionMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Altimiter
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[37] + fileContent[38], DewPointOptionMap)}.{getLabelFromValue(fileContent[39] + fileContent[40], DewPointOptionMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Runway Markings
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[41], RWYMarkingsOptionMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Aircraft Rescue and Firefighting
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[42], ARFFOptionMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td colSpan={2}>
+                        <table className='EndScreenTable' border={1}>
+                            <tr>
+                                <td>
+                                    Current On Ground
+                                </td>
+                                <td>
+                                    Parking Max on Ground
+                                </td>
+                                <td>
+                                    Working Max on Ground
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <span>{getLabelFromValue(fileContent[43], OnGroundOptionsMap)}</span>
+                                </td>
+                                <td>
+                                    <span>{getLabelFromValue(fileContent[44], OnGroundOptionsMap)}</span>
+                                </td>
+                                <td>
+                                    <span>{getLabelFromValue(fileContent[45], OnGroundOptionsMap)}</span>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Material Handeling Equip 1
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[46], MHETypeOptionsMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Equip 1 Quantity
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[47], MHENumberOptionsMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Material Handeling Equip 2
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[48], MHETypeOptionsMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Equip 1 Quantity
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[49], MHENumberOptionsMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Fuel Type 1
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[50], FuelTypeOptionsMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Fuel Quantity 1
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[51], FuelQtyOptionsMap)} lbs</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Fuel Type 2
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[52], FuelTypeOptionsMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Fuel Quantity 2
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[53], FuelQtyOptionsMap)} lbs</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Medical Level Available
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[54], MedLevelAvailOptionsMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Intigraded Threat Warning
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[55], ITWCapabilityOptionsMap)} minutes</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Frequency
+                    </td>
+                    <td>
+                        <span>{getLabelFromValue(fileContent[56], defaultOptionMap)}
+                            {getLabelFromValue(fileContent[57], defaultOptionMap)}
+                            {getLabelFromValue(fileContent[58], defaultOptionMap)}
+                            {getLabelFromValue(fileContent[59], defaultOptionMap)}
+                            {getLabelFromValue(fileContent[60], defaultOptionMap)}
+                            {getLabelFromValue(fileContent[61], defaultOptionMap)}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td colSpan={2}>
+                        <table className='EndScreenTable' border={1}>
+                            <tr>
+                                <td colSpan={5}>
+                                    Suitability Codes
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    {getLabelFromValue(fileContent[62], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[63], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[64], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[65], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[66], SuitabilityCodeOptionMap)}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    {getLabelFromValue(fileContent[67], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[68], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[69], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[70], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[71], SuitabilityCodeOptionMap)}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    {getLabelFromValue(fileContent[72], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[73], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[74], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[75], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[76], SuitabilityCodeOptionMap)}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    {getLabelFromValue(fileContent[77], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[78], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[79], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[80], SuitabilityCodeOptionMap)}
+                                </td>
+                                <td>
+                                    {getLabelFromValue(fileContent[81], SuitabilityCodeOptionMap)}
+                                </td>
+                            </tr>
+                        </table>
                     </td>
                 </tr>
             </tbody >
